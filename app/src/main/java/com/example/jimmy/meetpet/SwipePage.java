cf080c7320f35140874d4bc1f2b6466ffae7af13
jim044 <jimmy.villossel@gmail.com>
2018-12-22 11:28:30 +0100

    Second update

---
+++ app/src/main/java/com/example/jimmy/meetpet/SwipePage.java
@@ -0,0 +1,13 @@
+package com.example.jimmy.meetpet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SwipePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_swipe_page);
+    }
+}
